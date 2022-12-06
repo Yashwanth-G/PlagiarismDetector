383b8858b4d90308f51fc13bca7df4c23fae0367
Yashwanth Gundlapally <g.yashwanth896@gmail.com>
2022-12-05 20:11:04 -0500

    Final Changes with comments

--- src/PlagiarismDetector.java
+++ src/PlagiarismDetector.java
@@ -20,6 +20,11 @@ public class PlagiarismDetector {
     static String removePatternFound_2 = "";
     static String removePatternFound_3 = "";
 
+    /**
+     * The below method is used to take filePath and covert it into String in lowercase
+     * @param fileName
+     * @return
+     */
     public static String fileToString(String fileName) {
         String content = "";
         BufferedReader bufferedReader = null;
@@ -29,27 +34,11 @@ public class PlagiarismDetector {
             String line = null;
             String lineSeparator = System.getProperty("line.separator");
             while ((line = bufferedReader.readLine()) != null) {
-                //System.out.println("Before: "+line);
-//                line = line.replaceAll("[(){}\\\\]","");
-//                line = line.replaceAll("\\b(?i)an?\\b","");
-//                line = line.replaceAll("\\b(?i)the?\\b","");
-//                line = line.replaceAll("\\b(?i)and?\\b","");
-//                line = line.replaceAll("\\b(?i)or?\\b","");
-//                line = line.replaceAll("\\b(?i)to?\\b","");
-//                line = line.replaceAll("\\b(?i)or?\\b","");
-//                line = line.replaceAll("\\b(?i)is?\\b","");
-//                line = line.replaceAll("\\b(?i)are?\\b","");
-//                line = line.replaceAll("\\b(?i)i?\\b","");
-//                line = line.replaceAll("\\b(?i)this?\\b","");
-//                line = line.replaceAll("\\b(?i)there?\\b","");
-//                line = line.replaceAll(" +", " ");
-                //System.out.println("After: "+line);
                 stringBuilder.append(line);
                 stringBuilder.append(lineSeparator);
             }
             stringBuilder.deleteCharAt(stringBuilder.length() - 1);
             content = stringBuilder.toString();
-
         } catch (FileNotFoundException fileNotFoundException) {
             System.out.println(fileNotFoundException.getMessage());
         } catch (IOException ioException) {
@@ -65,72 +54,86 @@ public class PlagiarismDetector {
         }
     }
 
-    static void searchForThread_1(String string, String patternToSearch)
+    /**
+     * Search given word from one file in other file by thread-1
+     * @param string
+     * @param pattern
+     */
+    static void searchForThread_1(String string, String pattern)
     {
         int hashValueForString = 0;
         int hashValueForPattern = 0;
-        for (int i = 0; i < patternToSearch.length(); ++i) {
-            hashValueForString += patternToSearch.charAt(i) - 'a';
+        for (int i = 0; i < pattern.length(); ++i) {
+            hashValueForString += pattern.charAt(i) - 'a';
             hashValueForPattern += string.charAt(i) - 'a';
         }
         int j = 0;
-        for (int i = 0; i <= string.length() - patternToSearch.length(); ++i) {
+        for (int i = 0; i <= string.length() - pattern.length(); ++i) {
             if (hashValueForPattern == hashValueForString) {
-                for (j = 0; j < patternToSearch.length(); ++j) {
-                    if (patternToSearch.charAt(j)
+                for (j = 0; j < pattern.length(); ++j) {
+                    if (pattern.charAt(j)
                             != string.charAt(i + j)) {
                         break;
                     }
                 }
             }
-            if (j == patternToSearch.length()) {
+            if (j == pattern.length()) {
                 qwe1 = i;
                 arrayToStoreIndices_1.add(qwe1);
-                arrayToStorePattern_1.add(patternToSearch);
-                removePatternFound_1 = patternToSearch;
+                arrayToStorePattern_1.add(pattern);
+                removePatternFound_1 = pattern;
                 break;
             }
-            if (i == string.length() - patternToSearch.length())
+            if (i == string.length() - pattern.length())
                 break;
             hashValueForPattern = (int)((hashValueForPattern) - (string.charAt(i) - 'a'))
-                    + string.charAt(i + patternToSearch.length()) - 'a';
+                    + string.charAt(i + pattern.length()) - 'a';
         }
     }
 
-    static void searchForThread_2(String S2, String pattern2)
+    /**
+     * Search given word from one file in other file by thread-2
+     * @param string
+     * @param pattern
+     */
+    static void searchForThread_2(String string, String pattern)
     {
         int hashValueForString = 0;
         int hashValueForPattern = 0;
 
-        for (int i = 0; i < pattern2.length(); ++i) {
-            hashValueForString += pattern2.charAt(i) - 'a';
-            hashValueForPattern += S2.charAt(i) - 'a';
+        for (int i = 0; i < pattern.length(); ++i) {
+            hashValueForString += pattern.charAt(i) - 'a';
+            hashValueForPattern += string.charAt(i) - 'a';
         }
         int j = 0;
-        for (int i = 0; i <= S2.length() - pattern2.length(); ++i) {
+        for (int i = 0; i <= string.length() - pattern.length(); ++i) {
             if (hashValueForPattern == hashValueForString) {
-                for (j = 0; j < pattern2.length(); ++j) {
-                    if (pattern2.charAt(j)
-                            != S2.charAt(i + j)) {
+                for (j = 0; j < pattern.length(); ++j) {
+                    if (pattern.charAt(j)
+                            != string.charAt(i + j)) {
                         break;
                     }
                 }
             }
-            if (j == pattern2.length()) {
+            if (j == pattern.length()) {
                 qwe2 = i;
                 arrayToStoreIndices_2.add(qwe2);
-                arrayToStorePattern_2.add(pattern2);
-                removePatternFound_2 = pattern2;
+                arrayToStorePattern_2.add(pattern);
+                removePatternFound_2 = pattern;
                 break;
             }
-            if (i == S2.length() - pattern2.length())
+            if (i == string.length() - pattern.length())
                 break;
-            hashValueForPattern = (int)((hashValueForPattern) - (S2.charAt(i) - 'a'))
-                    + S2.charAt(i + pattern2.length()) - 'a';
+            hashValueForPattern = (int)((hashValueForPattern) - (string.charAt(i) - 'a'))
+                    + string.charAt(i + pattern.length()) - 'a';
         }
     }
 
-
+    /**
+     * Search given word from one file in other file by thread-3
+     * @param string
+     * @param pattern
+     */
     static void searchForThread_3(String string, String pattern)
     {
         int hashValueForString = 0;
@@ -164,31 +167,27 @@ public class PlagiarismDetector {
         }
     }
 
+    /**
+     * The application execution starts from here
+     * @param args
+     * @throws IOException
+     * @throws InterruptedException
+     */
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        int q = 109;
+//        String file1 = "C:\\Users\\gyash\\OneDrive\\Desktop\\COMP6651\\sample_data_and_submission\\data\\okay06\\1.txt";
+//        String file2 = "C:\\Users\\gyash\\OneDrive\\Desktop\\COMP6651\\sample_data_and_submission\\data\\okay06\\2.txt";
 
-        String file1 = "C:\\Users\\gyash\\OneDrive\\Desktop\\COMP6651\\sample_data_and_submission\\data\\okay09\\1.txt";
-        String file2 = "C:\\Users\\gyash\\OneDrive\\Desktop\\COMP6651\\sample_data_and_submission\\data\\okay09\\2.txt";
+        String file1 = "C:\\Users\\gyash\\OneDrive\\Desktop\\COMP6651\\sample_data_and_submission\\data\\plagiarism07\\1.txt";
+        String file2 = "C:\\Users\\gyash\\OneDrive\\Desktop\\COMP6651\\sample_data_and_submission\\data\\plagiarism07\\2.txt";
 
-//        String file1 = "C:\\Users\\gyash\\OneDrive\\Desktop\\COMP6651\\sample_data_and_submission\\data\\plagiarism06\\1.txt";
-//        String file2 = "C:\\Users\\gyash\\OneDrive\\Desktop\\COMP6651\\sample_data_and_submission\\data\\plagiarism06\\2.txt";
+//        String fileName1 = args[0];
+//
+//        String fileName2 = args[1];
 
-        long start1 = System.nanoTime();
+        String s1 = fileToString(file1);
 
-        String fileName1 = args[0];
-
-        String fileName2 = args[1];
-
-        String s1 = fileToString(fileName1);
-        long end1 = System.nanoTime();
-        double elapsedTimeInSecond1 = (double) (end1 - start1) / 1_000_000_000;
-
-        long start2 = System.nanoTime();
-        String s2 = fileToString(fileName2);
-        long end2 = System.nanoTime();
-        double elapsedTimeInSecond2 = (double) (end2 - start2) / 1_000_000_000;
-        //System.out.println(elapsedTimeInSecond2);
+        String s2 = fileToString(file2);
 
         String backup = s1;
 
@@ -202,6 +201,10 @@ public class PlagiarismDetector {
 
         StringBuilder sb3 = new StringBuilder(s2);
 
+        /**
+         * Given files are splitted into three parts and each part is handled by one thread
+         * first part of the file is executed by thread-1
+         */
         Thread t1 = new Thread(() -> {
             for(int i = 0 ; i < words1.length / 3; i++){
                 if(removePatternFound_1.length()>0)
@@ -211,6 +214,9 @@ public class PlagiarismDetector {
             }
         });
 
+        /**
+         * Second part of the files is executed by thread-2
+         */
         Thread t2 = new Thread(() -> {
             for(int i = words1.length / 3 ; i < words1.length / 1.5 ; i++){
                 if(removePatternFound_2.length()>0)
@@ -220,6 +226,9 @@ public class PlagiarismDetector {
             }
         });
 
+        /**
+         * Third part of the files is executed by thread-3
+         */
         Thread t3 = new Thread(() -> {
             for(int i = (int)(words1.length / 1.5) ; i < words1.length ; i++){
                 if(removePatternFound_3.length()>0)
@@ -229,17 +238,12 @@ public class PlagiarismDetector {
             }
         });
 
-        long start3 = System.nanoTime();
         t1.start();
         t2.start();
         t3.start();
         t1.join();
         t2.join();
         t3.join();
-        long end3 = System.nanoTime();
-        double elapsedTimeInSecond3 = (double) (end3 - start3) / 1_000_000_000;
-
-//        System.out.println("Searching: "+elapsedTimeInSecond3);
 
         Thread t5 = new Thread(() -> {
             arrayToStoreIndices_1.addAll(arrayToStoreIndices_2);
@@ -250,17 +254,19 @@ public class PlagiarismDetector {
             arrayToStorePattern_1.addAll(arrayToStorePattern_2);
             arrayToStorePattern_1.addAll(arrayToStorePattern_3);
         });
-        long start4 = System.nanoTime();
         t5.start();
         t6.start();
         t5.join();
         t6.join();
 
-        long end4 = System.nanoTime();
-        double elapsedTimeInSecond4 = (double) (end4 - start4) / 1_000_000_000;
-//        System.out.println("Merging time: "+elapsedTimeInSecond4);
 
-        long start5 = System.nanoTime();
+        /**
+         * Using the above arraylists the Similarity score is calculated in the below lines of code
+         * The indicies of all the matched words are stored in one list and other array list contains the pattern of words that are
+         * matched. By iterating on the indices array list sentenced is formed by traversing all the increasing indices and the sentences
+         *  should be more than or equal to 2 words if they are more than or equal to words than the count of the owrds is added to global
+         *  variable called max which is used to calculate the similarity score.
+         */
         int max = 0;
         String str = arrayToStorePattern_1.get(0) + " ";
         double sqrt = Math.sqrt(words1.length * words2.length);
@@ -273,7 +279,7 @@ public class PlagiarismDetector {
                     if(i == arrayToStoreIndices_1.size()-1 && !str.equals(" ")) {
                         max += str.trim().split(" ").length;
                         score = ((double)max/ (double) sqrt * 100);
-                        if(score > 46.0){
+                        if(score > 42.0){
                             break;
                         }
                     }
@@ -282,7 +288,7 @@ public class PlagiarismDetector {
                     max += str.trim().split(" ").length;
                     str = " ";
                     score = ((double)max/ (double) sqrt * 100);
-                    if(score > 46.0){
+                    if(score > 42.0){
                         break;
                     }
                 }
@@ -290,18 +296,10 @@ public class PlagiarismDetector {
         }
         DecimalFormat df = new DecimalFormat("#.###");
         score = Double.parseDouble(df.format(score));
-        long end5 = System.nanoTime();
-        double elapsedTimeInSecond5 = (double) (end5 - start5) / 1_000_000_000;
-//        System.out.println("Score Calculation Time: "+elapsedTimeInSecond5);
 
-        double totalExe = elapsedTimeInSecond2 + elapsedTimeInSecond2 + elapsedTimeInSecond3 + elapsedTimeInSecond4 + elapsedTimeInSecond5;
-//        System.out.println("Total Time: "+totalExe);
-
-//        System.out.println("Similarity: "+score);
-        if(score > 45)
+        if(score > 42.0)
             System.out.println("1");
         else
             System.out.println("0");
-
     }
 }
